443f29546f1ea644c7e91027b8f4e9e2629ac4f1
v1rtl <pilll.pl22@gmail.com>
2021-01-05 21:16:59 +0300

    feat: use official rollup plugin instead

--- packages/hyperlight/rollup.config.js
+++ packages/hyperlight/rollup.config.js
@@ -1,26 +1,24 @@
-import esbuild from 'rollup-plugin-esbuild'
-import pluginBin from 'rollup-plugin-bin'
-import plugints from 'rollup-plugin-ts'
-
-import pkg from './package.json'
-
-const config = (input, output, plugins) => ({
-  input,
-  plugins: [plugints({ tsconfig: 'tsconfig.json' }), esbuild(), ...plugins],
-  output: output ?? {
-    dir: 'dist',
-    format: 'esm'
-  }
-})
+import common from '../../rollup.config'
 
 export default [
-  config('src/hyperlight.ts', undefined, [pluginBin()]),
-  config(
-    'src/index.ts',
-    [
-      { format: 'esm', file: pkg.module },
-      { format: 'cjs', file: pkg.main }
-    ],
-    []
-  )
+  {
+    ...common,
+    input: 'src/index.ts',
+    output: [
+      {
+        dir: 'dist',
+        format: 'esm'
+      }
+    ]
+  },
+  {
+    ...common,
+    input: 'src/hyperlight.ts',
+    output: [
+      {
+        file: 'dist/hyperlight.js',
+        format: 'esm'
+      }
+    ]
+  }
 ]
---
+++ rollup.config.js
@@ -0,0 +1,12 @@
+import typescript from '@rollup/plugin-typescript'
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      dir: 'dist',
+      format: 'esm'
+    }
+  ],
+  plugins: [typescript({ include: ['./src/**/*.ts'] })]
+}
