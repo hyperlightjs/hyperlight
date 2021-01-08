e4853f0c424f4e52235529e86a011da145c7f8c3
v1rtl <pilll.pl22@gmail.com>
2021-01-08 16:11:39 +0300

    feat: use proper console methods for logger

--- packages/hyperlight/src/utils/logging.ts
+++ packages/hyperlight/src/utils/logging.ts
@@ -3,13 +3,13 @@ import { bgCyan, bold, black, bgRed, bgYellow } from 'colorette'
 const boldBlack = (text: string) => bold(black(text))
 
 export function info(message: string) {
-  console.log(`${boldBlack(bgCyan(' INFO '))} ${message}`)
+  console.info(`${boldBlack(bgCyan(' INFO '))} ${message}`)
 }
 
 export function error(message: string) {
-  console.log(`${bgRed(' ERROR ')} ${message}`)
+  console.error(`${bgRed(' ERROR ')} ${message}`)
 }
 
 export function warning(message: string) {
-  console.log(`${boldBlack(bgYellow(' WARN '))} ${message}`)
+  console.warn(`${boldBlack(bgYellow(' WARN '))} ${message}`)
 }
